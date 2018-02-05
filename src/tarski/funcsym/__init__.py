b2c146c135dd02810a0a79ebfbd0394b52f3da12
Guillem Frances <guillem.frances@gmail.com>
2018-02-05 13:32:57 +0100

    Code Style

--- src/tarski/funcsym/__init__.py
+++ src/tarski/funcsym/__init__.py
@@ -5,41 +5,39 @@ from ._arithmetic import Addition, Subtraction, Multiplication, Division, Modulo
     Max, Min, StandardLibFunction
 
 symbols = [
-    # 0-ary
-    {}, \
-    # 1-ary
-    { }, \
-    # binary
+    {},  # 0-ary
+    {},  # 1-ary
+    # binary:
     {
-        '+' : Addition,
-        '-' : Subtraction,
-        '*' : Multiplication,
-        '/' : Division,
-        '%' : Modulo,
-        '**' : Power,
-        'max' : Max,
-        'min' : Min,
-        'atan2' : ArcTangent2
+        '+': Addition,
+        '-': Subtraction,
+        '*': Multiplication,
+        '/': Division,
+        '%': Modulo,
+        '**': Power,
+        'max': Max,
+        'min': Min,
+        'atan2': ArcTangent2
 
     }
 ]
 
-def initialize( L  ) :
 
+def initialize(L):
     # register real defined functions
-    L.register_symbol( ('sqrt', L.Real), StandardLibFunction( 'sqrt', L, L.Real) )
-    L.register_symbol( ('sin', L.Real), StandardLibFunction( 'sin', L, L.Real) )
-    L.register_symbol( ('cos', L.Real), StandardLibFunction( 'cos', L, L.Real) )
-    L.register_symbol( ('tan', L.Real), StandardLibFunction( 'tan', L, L.Real) )
-    L.register_symbol( ('asin', L.Real), StandardLibFunction( 'asin', L, L.Real) )
-    L.register_symbol( ('acos', L.Real), StandardLibFunction( 'acos', L, L.Real) )
-    L.register_symbol( ('atan', L.Real), StandardLibFunction( 'atan', L, L.Real) )
-    L.register_symbol( ('exp', L.Real), StandardLibFunction( 'exp', L, L.Real) )
-    L.register_symbol( ('abs', L.Real), StandardLibFunction( 'abs', L, L.Real) )
+    L.register_symbol(('sqrt', L.Real), StandardLibFunction('sqrt', L, L.Real))
+    L.register_symbol(('sin', L.Real), StandardLibFunction('sin', L, L.Real))
+    L.register_symbol(('cos', L.Real), StandardLibFunction('cos', L, L.Real))
+    L.register_symbol(('tan', L.Real), StandardLibFunction('tan', L, L.Real))
+    L.register_symbol(('asin', L.Real), StandardLibFunction('asin', L, L.Real))
+    L.register_symbol(('acos', L.Real), StandardLibFunction('acos', L, L.Real))
+    L.register_symbol(('atan', L.Real), StandardLibFunction('atan', L, L.Real))
+    L.register_symbol(('exp', L.Real), StandardLibFunction('exp', L, L.Real))
+    L.register_symbol(('abs', L.Real), StandardLibFunction('abs', L, L.Real))
 
     # register arithmetic functions
-    sorts = [ L.Real, L.Integer, L.Natural ]
+    sorts = [L.Real, L.Integer, L.Natural]
 
-    for sym, klass in symbols[2].items() :
-        for s in sorts :
-            L.register_symbol( (sym, s, s), klass(L,s,s))
+    for sym, klass in symbols[2].items():
+        for s in sorts:
+            L.register_symbol((sym, s, s), klass(L, s, s))
