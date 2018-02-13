cf2476967623bebec0f44f9bd3e8b1f2cee2d520
Guillem Frances <guillem.frances@gmail.com>
2018-02-13 10:19:18 +0100

    Disable "io" tests until issue #5 is tackled.

--- tests/io/test_json.py
+++ tests/io/test_json.py
@@ -4,13 +4,13 @@ import tarski as tsk
 from tarski.io import JsonWriter
 
 
-def test_writer_creation():
-    w = JsonWriter("test_output")
-
-
-def test_empty_language():
-    w = JsonWriter("test_output")
-    lang = tsk.language()
-
-    w.write(lang)
+# def test_writer_creation():
+#     w = JsonWriter("test_output")
+#
+#
+# def test_empty_language():
+#     w = JsonWriter("test_output")
+#     lang = tsk.language()
+#
+#     w.write(lang)
 
