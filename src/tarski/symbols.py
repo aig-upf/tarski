a40190a80565da8b740575e5eb9c55d6043cc6da
miquel.ramirez@gmail.com <miquel.ramirez@gmail.com>
2018-02-19 14:02:59 +1100

    Corrected issue with relative paths of modules

--- src/tarski/symbols.py
+++ src/tarski/symbols.py
@@ -1,3 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from .formulas import axiom, top, bot, land, lor, neg, exists, forall, implies
+from .syntax.formulas import top, bot, land, lor, neg, exists, forall, implies
