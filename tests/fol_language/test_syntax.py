fe595a8b9b979e0aca598c1a6205d64cb3a64dd6
miquel.ramirez@gmail.com <miquel.ramirez@gmail.com>
2018-02-19 15:15:29 +1100

    Test Synchronization
     - Changed test to use load_module() rather than imports
     - Added new test case to exercise limitations on loading language
    modules

--- tests/fol_language/test_syntax.py
+++ tests/fol_language/test_syntax.py
@@ -16,12 +16,24 @@ def test_arithmetic_terms_fails_without_import():
     with pytest.raises(TypeError):
         # sum_ = two + three should raise TypeError as no import has been made of the arithmetic module
         sum_ = two + three
-        
 
-def test_arithmetic_terms_does_not_fail_with_import():
+
+def test_arithmetic_terms_does_not_fail_with_load_module():
     import tarski.syntax.arithmetic
     lang = tsk.language()
+    lang.load_module('arithmetic')
     ints = lang.Integer
     two, three = lang.constant(2, ints), lang.constant(3, ints)
     sum_ = two + three
     assert isinstance(sum_, tsk.Term), "sum_ should be the term +(Const(2), Const(3)), not the integer value 5"
+
+def test_load_arithmetic_module_fails_when_language_frozen() :
+    import tarski.errors as err
+    lang = tsk.language()
+    ints = lang.Integer
+    two, three = lang.constant(2, ints), lang.constant(3, ints)
+
+    with pytest.raises(err.LanguageError) :
+        # load_module() should raise LanguageError as we have created two constants
+        lang.load_module('arithmetic')
+        sum_ = two + three
