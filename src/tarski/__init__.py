d0065ecada0670064cca976040f8e698ae0d4530
Miquel Ramírez <miquel.ramirez@gmail.com>
2018-02-21 23:13:45 +1100

    Tarski does bunches of stuff

--- src/tarski/__init__.py
+++ src/tarski/__init__.py
@@ -10,3 +10,11 @@ def language(name = 'L'):
     lang = FirstOrderLanguage(name)
     funcsym.initialize(lang)
     return lang
+
+class Bunch(object):
+    """
+    A Bunch of stuff. Handy class for grouping together
+    stuff.
+    """
+    def __init__(self, **kwargs):
+        self.__dict__.update(kwargs)
