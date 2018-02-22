48d167f233224e1195babe9a64324695e4cd7d40
miquel.ramirez@gmail.com <miquel.ramirez@gmail.com>
2018-02-22 15:42:45 +1100

    FSTRIPS Problem
     - Improved usability of constructor
     - Errors are now imported from the FSTRIPS specific module

--- src/tarski/fstrips/fstrips.py
+++ src/tarski/fstrips/fstrips.py
@@ -1,8 +1,7 @@
 # -*- coding: utf-8 -*-
 from collections import OrderedDict
 
-from ..errors import DuplicateActionDefinition, UndefinedAction
-
+from . import errors as err
 
 # class Action(abc.ABC):
 class Action(object):
@@ -30,21 +29,46 @@ class Action(object):
 class Problem(object):
     """ A Functional STRIPS problem """
 
-    def __init__(self, name, language, init, goal, actions=None):
+    def __init__(self, name, language, **kwargs):
+        """
+        A FSTRIPS Problem description. Constructor parameters are:
+
+         - name: the name of the problem
+         - language: FOL associated with the planning problem
+         - init: initial state of the problem (required)
+         - goal: formula specifying the set of goal states
+         - constraints: the set of constraints that valid plans need to comply with
+         before reaching a goal state
+        """
         self._name = name
         self.language = language
-        self.init = init
-        self.goal = goal
+        try :
+            self.init = kwargs['init']
+            if self.init is None :
+                raise err.IncompleteProblemError(self,msg="No initial state was given")    
+        except KeyError:
+            raise err.IncompleteProblemError(self,msg="No initial state was given")
+        try :
+            self.goal = kwargs['goal']
+        except KeyError:
+            raise err.IncompleteProblemError(self,msg="Goal was not specified")
 
+        self.constraints = kwargs.get('constraints',None)
+        actions = [ ( (a.name,tuple(a.parameters)) , a) for a in kwargs.get('actions',[])]
         self.actions = OrderedDict(actions) if actions is not None else OrderedDict()
 
         # TODO Add axioms, state constraints, etc.
 
+    @property
+    def name(self):
+        return self._name
+
     def action(self, name, parameters, precondition, effects):
         if name in self.actions:
-            raise DuplicateActionDefinition(name, self.actions[name])
+            # @TODO: MRJ: check action signature, rather than just the name
+            raise err.DuplicateActionDefinition(name, self.actions[name])
 
-        self.actions[name] = Action(self.language, name, parameters, precondition, effects)
+        self.actions[(name,tuple(parameters))] = Action(self.language, name, parameters, precondition, effects)
         return self.actions[name]
 
     def has_action(self, name):
@@ -52,5 +76,5 @@ class Problem(object):
 
     def get_action(self, name):
         if not self.has_action(name):
-            raise UndefinedAction(name)
+            raise err.UndefinedAction(name)
         return self.actions[name]
