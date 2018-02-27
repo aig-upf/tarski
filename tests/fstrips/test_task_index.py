8a0d5d6b4e790111f20347aad57d2e80a39026a1
Miquel Ramírez <miquel.ramirez@gmail.com>
2018-02-27 23:00:27 +1100

    TaskIndex unit tests

---
+++ tests/fstrips/test_task_index.py
@@ -0,0 +1,39 @@
+
+from tarski import fstrips as fs
+from tarski.syntax import *
+
+from ..common import blocksworld
+
+
+def test_task_index_constructor():
+
+    prob = blocksworld.create_small_task()
+    index = fs.TaskIndex(prob.language.name,prob.name)
+
+    assert index.domain_name == prob.language.name
+    assert index.instance_name == prob.name
+
+def test_task_index_process_symbols_fluents():
+    prob = blocksworld.create_small_task()
+    index = fs.TaskIndex(prob.language.name,prob.name)
+    index.process_symbols(prob)
+
+    print(','.join([str(sym) for sym in index.fluent_symbols]))
+    assert len(index.fluent_symbols) == 2
+
+def test_task_index_process_symbols_statics():
+    prob = blocksworld.create_small_task()
+    index = fs.TaskIndex(prob.language.name,prob.name)
+    index.process_symbols(prob)
+
+    print(','.join([str(sym) for sym in index.static_symbols]))
+    assert len(index.static_symbols) == 0
+
+def test_task_index_create_state_variables():
+    prob = blocksworld.create_small_task()
+    index = fs.TaskIndex(prob.language.name,prob.name)
+    index.process_symbols(prob)
+    index.process_state_variables()
+
+    print(','.join([str(var) for var in index.state_variables]))
+    assert len(index.state_variables) == 8
