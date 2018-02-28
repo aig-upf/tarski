8c44bf75d908b5ce652216f6cd8bf8f2420fedf4
Miquel Ramírez <miquel.ramirez@gmail.com>
2018-02-28 22:44:51 +1100

    Grounding Errors
     - Added UnableToGroundError

---
+++ src/tarski/grounding/errors.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+from .. errors import TarskiError, DuplicateDefinition, UndefinedElement
+
+class UnableToGroundError(TarskiError):
+
+    def __init__(self, sym, msg=None):
+        msg = msg or 'Reason unspecified'
+        super().__init__('Unable to ground Term/Atom "{}": {}'.format(str(sym),msg))
