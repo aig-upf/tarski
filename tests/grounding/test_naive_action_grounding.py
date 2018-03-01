1cf9077b5b11468f99de24de58cf280026e021db
miquel.ramirez@gmail.com <miquel.ramirez@gmail.com>
2018-03-01 17:36:33 +1100

    Action Grounding Tests

--- tests/grounding/test_naive_action_grounding.py
+++ tests/grounding/test_naive_action_grounding.py
@@ -1,6 +1,7 @@
 from tarski import fstrips as fs
 from tarski.grounding.naive import state_variables as sv
 from tarski.grounding.naive import instantiation
+from tarski.syntax.transform import TermSubstitution
 from tarski.syntax import *
 from tarski.util import IndexDictionary
 
@@ -37,3 +38,31 @@ def test_generate_substitutions_for_small_bw():
         assert(len(syms)==len(values))
         subst = { syms[k] : v for k,v in enumerate(values) }
         assert len(subst)==2
+
+def test_ground_actions_for_small_bw():
+    import itertools, copy
+
+    prob, index = create_small_bw_with_index()
+    index.ground_actions = IndexDictionary()
+    actions = list(prob.actions.values())
+    K, syms, substs = instantiation.enumerate(prob.language,actions[0].parameters)
+    ground_actions = []
+    for values in itertools.product(*substs):
+        subst = { syms[k] : v for k,v in enumerate(values) }
+        g_prec = copy.deepcopy(actions[0].precondition)
+        op = TermSubstitution(prob.language,subst)
+        g_prec.accept(op)
+        var_collector = CollectVariables(prob.language)
+        g_prec.accept(var_collector)
+        assert len(var_collector.variables) == 0
+        g_effs = []
+        for eff in grounded.effects:
+            g_eff = copy.deepcopy(eff)
+            g_eff.accept(op)
+            var_collector = CollectVariables(prob.language)
+            g_eff.accept(var_collector)
+            assert len(var_collector.variables) == 0
+            g_effs.append(g_eff)
+        ground_actions.append(fs.Action(prob.language, actions[0].name, [], g_prec, g_effs))
+        print(str(ground_actions[-1]))
+        assert False
