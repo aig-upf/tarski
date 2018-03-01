b87c27c04e7bdb6f1be062dfa86df3660941409b
Miquel Ramírez <miquel.ramirez@gmail.com>
2018-03-01 22:14:04 +1100

    Removed debuggin message

--- src/tarski/grounding/naive/instantiation.py
+++ src/tarski/grounding/naive/instantiation.py
@@ -14,7 +14,6 @@ def enumerate( L : FirstOrderLanguage, symbols ):
                 raise UnableToGroundError(st, "Term is of built-in sort '{}', domain is too large!".format(st.sort.name))
             syms.append(st)
             instantiations.append(list(st.sort.domain()))
-            print(len(instantiations[-1]))
             cardinality *= len(instantiations[-1])
         else :
             raise UnableToGroundError(st, "Grounding of complex nested subterms is not implemented yet!")
