27dc757599198dd1c12461590db4178f7b0bb30a
Miquel Ramírez <miquel.ramirez@gmail.com>
2018-03-08 19:51:47 +1100

    Naive State Variable Calculation
     - Harnessed ParcPrinter with tests

--- tests/grounding/test_naive_state_variables.py
+++ tests/grounding/test_naive_state_variables.py
@@ -9,9 +9,24 @@ from tests.common import parcprinter
 
 def test_task_static_symbol_detection():
     prob = parcprinter.create_small_task()
-    assert False
+    index = fs.TaskIndex(prob.language.name,prob.name)
+    index.process_symbols(prob)
+    assert len(index.static_symbols) == 0
+    assert len(index.fluent_symbols) == 4
 
-def test_task_index_create_state_variables():
+def test_task_index_create_state_variables_parcprinter():
+    prob = parcprinter.create_small_task()
+    index = fs.TaskIndex(prob.language,prob.name)
+    index.process_symbols(prob)
+    index.state_variables = IndexDictionary()
+    for var in sv.create_all_possible_state_variables(index.fluent_symbols):
+        index.state_variables.add(var)
+
+    print(','.join([str(var) for var in index.state_variables]))
+    assert len(index.state_variables) == 7
+
+
+def test_task_index_create_state_variables_blocksworld():
     prob = blocksworld.create_small_task()
     index = fs.TaskIndex(prob.language.name,prob.name)
     index.process_symbols(prob)
