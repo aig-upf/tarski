53daa60ac82a4850dc01373a9f9db576f23f7112
miquel.ramirez@gmail.com <miquel.ramirez@gmail.com>
2018-06-06 13:06:10 +0200

    Predicate Symbol Exported

--- src/tarski/__init__.py
+++ src/tarski/__init__.py
@@ -3,11 +3,8 @@
 import logging
 from .fol import FirstOrderLanguage
 from .theories import language
-from .syntax import Function, Formula, Term, Constant, Variable
+from .syntax import Function, Predicate, Formula, Term, Constant, Variable
 from .errors import LanguageError
 from . import fstrips
 
 logging.getLogger(__name__).addHandler(logging.NullHandler())
-
-
-
