659d586978d270ac556c5b8f867d164c2b9b7603
miquel.ramirez@gmail.com <miquel.ramirez@gmail.com>
2018-06-07 11:42:49 +0200

    Planning tasks mockups relocated

--- tests/common/parcprinter.py
+++ tests/common/parcprinter.py
@@ -1,5 +1,5 @@
 """
-    Generate blocksworld language elements
+    Generate parcprinter language elements
 """
 import tarski as tsk
 import tarski.model
@@ -7,47 +7,10 @@ import tarski.model
 from tarski import fstrips as fs
 from tarski.syntax import *
 from tarski.syntax.temporal import ltl
+from tarski.theories import Theory
 
-from tarski.evaluators.simple import evaluate
-
-""" Fragment of ParcPrinter relevant for testing
-
-(:types
-    sheet_t resource_t location_t
-)
-
-(:constants
-    Finisher2-RSRC - resource_t
-    Some_Finisher_Tray Finisher2_Entry-Finisher1_Exit Finisher2_Tray - location_t
-)
-
-(:predicates
-
-
-)
-
-(:action Finisher2-Stack-Letter
- :parameters ( ?sheet - sheet_t ?prevsheet - sheet_t)
- :precondition (and
-		(Available Finisher2-RSRC)
-		(Prevsheet ?sheet ?prevsheet)
-		(Location ?prevsheet Some_Finisher_Tray)
-		(Sheetsize ?sheet Letter)
-		(Location ?sheet Finisher2_Entry-Finisher1_Exit))
- :effect (and
-		(not (Available Finisher2-RSRC))
-		(Location ?sheet Some_Finisher_Tray)
-		(Stackedin ?sheet Finisher2_Tray)
-		(not (Location ?sheet Finisher2_Entry-Finisher1_Exit))
-		(Available Finisher2-RSRC)
-		(increase (total-cost) 8000))
-)
-
-)
-"""
-
-def create_parcprinter_language():
-    upp = tsk.language()
+def create_small_language():
+    upp = tsk.fstrips.language("upp", theories=[Theory.EQUALITY])
 
     upp.sheet_t  = upp.sort('sheet_t')
     upp.resource_t = upp.sort('resource_t')
@@ -69,40 +32,3 @@ def create_parcprinter_language():
     upp.Uninitialized = upp.predicate('Uninitialized')
 
     return upp
-
-def create_small_task():
-
-    upp = create_parcprinter_language()
-    dummy_sheet = upp.constant('dummy-sheet', upp.sheet_t)
-    sheet1 = upp.constant('sheet1', upp.sheet_t)
-
-
-    M = tsk.model.create(upp)
-    M.evaluator = evaluate
-    M.add( upp.Uninitialized )
-    M.add( upp.Location, dummy_sheet, upp.some_finisher_tray)
-    M.add( upp.Prevsheet, sheet1, dummy_sheet)
-    M.add( upp.Sheetsize, sheet1, upp.letter)
-    M.add( upp.Location, sheet1, upp.some_feeder_tray)
-
-    sheet = upp.variable('sheet', upp.sheet_t)
-    prevsheet = upp.variable('prevsheet', upp.sheet_t)
-
-    action = fs.Action( upp, name='Finisher2-Stack-Letter',\
-                        parameters = [sheet, prevsheet],\
-                        precondition = land( upp.Available(upp.finisher2_rsrc),\
-                                            upp.Prevsheet(sheet, prevsheet),\
-                                            upp.Location(prevsheet,upp.some_finisher_tray),\
-                                            upp.Sheetsize(sheet, upp.letter),\
-                                            upp.Location(sheet, upp.finisher2_entry_finisher1_exit)),
-                        # MRJ: note that the positive effects should be under an extra X op, as per the
-                        # "deletes before adds" semantics,
-                        effects = [ ltl.X( land(\
-                                            neg(upp.Available(upp.finisher2_rsrc)),\
-                                            neg(upp.Location(sheet, upp.finisher2_entry_finisher1_exit)),\
-                                            ltl.X( land(\
-                                            upp.Location(sheet, upp.some_finisher_tray),\
-                                            upp.Stackedin(sheet, upp.finisher2_tray),\
-                                            upp.Available(upp.finisher2_rsrc)))))])
-    P = fs.Problem("fun", upp, init=M, goal=top, constraints = [], actions=[action])
-    return P
