05f6de4cded78a5d250c1aafe0ef1d838b17b716
miquel.ramirez@gmail.com <miquel.ramirez@gmail.com>
2018-07-26 10:36:37 +1000

    New language: Two dimensional billiards

--- tests/common/numeric.py
+++ tests/common/numeric.py
@@ -20,3 +20,37 @@ def generate_numeric_instance():
         lang.constant('p{}'.format(k), particle)
 
     return lang
+
+def generate_billiards_instance():
+
+    lang = tsk.fstrips.language(theories=[Theory.EQUALITY, Theory.ARITHMETIC])
+
+    # The sorts
+    ball = lang.sort('ball')
+    dim = lang.sort('dimension')
+    force = lang.sort('force')
+
+    # The stuff
+    lang.balls = []
+    for k in (1,2):
+        bk = lang.constant('ball_{}'.format(k), ball)
+        lang.balls.append(bk)
+
+    lang.dimensions = []
+    for d in ('x','y')
+        dk = lang.constant(d, dim)
+        lang.dimensions.append(dk)
+
+    lang.forces = []
+    for n in ('cue', 'friction'):
+        fk = lang.constant(n, force)
+        lang.forces.append(fk)
+
+    # The properties of stuff
+    m = lang.function('m', ball, lang.Real)
+    F = lang.function('F', force, dim, ball, lang.Real)
+    a = lang.function('a', dim, ball, lang.Real)
+    v = lang.function('v', dim, ball, lang.Real)
+    p = lang.function('p', dim, ball, lang.Real)
+
+    return lang
