f3c44adfde35f44a53431407a746963ccb6f109a
miquel.ramirez@gmail.com <miquel.ramirez@gmail.com>
2018-08-07 12:40:09 +1000

    Interface tweaking
     - SingleEffect base class is now exposed for being extended by planners

--- src/tarski/fstrips/__init__.py
+++ src/tarski/fstrips/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from .problem import Problem, create_fstrips_problem
 from .action import Action
-from .fstrips import AddEffect, DelEffect, FunctionalEffect, UniversalEffect, \
+from .fstrips import SingleEffect, AddEffect, DelEffect, FunctionalEffect, UniversalEffect, \
     LogicalEffect, ChoiceEffect, language, OptimizationMetric, OptimizationType
 from .task_index import TaskIndex
