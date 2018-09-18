ef40e958147cec75813468b4b1a1d823d6998c91
Miquel Ramírez <miquel.ramirez@gmail.com>
2018-09-19 08:31:38 +1000

    Updated parser files

--- src/tarski/io/_fstrips/parser/visitor.py
+++ src/tarski/io/_fstrips/parser/visitor.py
@@ -1,4 +1,4 @@
-# Generated from /home/bowman/Sandboxes/tarski/utils/parsers/grammars/fstrips.g4 by ANTLR 4.7.1
+# Generated from /mnt/c/Sandboxes/tarski/utils/parsers/grammars/fstrips.g4 by ANTLR 4.7.1
 from antlr4 import *
 if __name__ is not None and "." in __name__:
     from .parser import fstripsParser
