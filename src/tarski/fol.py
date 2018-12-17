3f04017f05ed2ff35a66ea38ab0774a05cb11072
Miquel Ramírez <miquel.ramirez@gmail.com>
2018-12-17 16:05:46 +1100

    Update fol.py
    - ite function is now marked as a builtin

--- src/tarski/fol.py
+++ src/tarski/fol.py
@@ -45,7 +45,8 @@ class FirstOrderLanguage:
 
         self._build_builtin_sorts()
         # "virtual function" ite (if-then-else)
-        self.function('ite', self.Object, self.Object, self.Object)
+        ite_func = self.function('ite', self.Object, self.Object, self.Object)
+        ite_func.builtin = True
 
 
     def __deepcopy__(self, memo):
