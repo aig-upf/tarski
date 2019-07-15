cf8e78ff8d2c0c29ab9a2af7908c70c004940c46
Guillem Frances <guillem.frances@gmail.com>
2019-07-15 17:51:47 +0200

    Minor fixes

--- src/tarski/sdd/sdd.py
+++ src/tarski/sdd/sdd.py
@@ -21,21 +21,21 @@ class MaxSizeError(Exception):
     pass
 
 
-def scout_actions(task, data_df):
+def scout_actions(task, data):
+    """ Check & report which actions have (potentially) some groundings, and which not """
+    # TODO: This will probably fail on parameter-less actions??
     actions, empty = [], []
-    for _, action in task.actions.items():
+    for action in task.actions.values():
         cardinalities = [len(list(x.sort.domain())) for x in action.parameters]
-        count = reduce(operator.mul, cardinalities, 1)
-        logging.debug('{}:\t{} = {} potential groundings'.format(action.ident(),
-                                                                 " * ".join(map(str, cardinalities)), count))
-        if count == 0:
+        ngroundings = reduce(operator.mul, cardinalities, 1)
+        logging.debug(f'{action.ident()}:\t{" * ".join(map(str, cardinalities))} = {ngroundings} potential groundings')
+        if ngroundings == 0:
             empty.append(action.name)
-            continue
-
-        actions.append(action)
-        data_df['action'] += [action.ident()]
-        data_df['arity'] += [len(action.parameters)]
-        data_df['groundings'] += [count]
+        else:
+            actions.append(action)
+            data['action'] += [action.ident()]
+            data['arity'] += [len(action.parameters)]
+            data['groundings'] += [ngroundings]
 
     logging.debug(f"{len(actions)} actions to ground; actions discarded: {empty}")
     return actions
@@ -69,16 +69,15 @@ def extract_regular_atoms(phi, atoms):
         raise RuntimeError('Unsupported formula: {}'.format(str(phi)))
 
 
-def create_equality_constraints(equalities, selects, eq_constraints):
+def create_equality_constraints(equalities, selects):
+    """ Return a list of equality constraints of the form """
+    eq_constraints = []
     for s, p in equalities.items():
-        # print(s.expr.subterms)
         lhs, rhs = s.expr.subterms
         sel_lhs = selects[symref(lhs)]
         sel_rhs = selects[symref(rhs)]
         for l, r in itertools.product(*[sel_lhs, sel_rhs]):
             if l.subterms[-1].symbol == r.subterms[-1].symbol:
-                if not p:
-                    eq_constraints += [lor(neg(l), neg(r))]
                 if p:
                     eq_constraints += [lor(neg(l), r), lor(neg(r), l)]
                     # Let us have a strong abort here to inspect preconditions of this type (i.e.: a precondition X=Y
@@ -86,6 +85,8 @@ def create_equality_constraints(equalities, selects, eq_constraints):
                     # surprising to me. If they indeed occur in practice, it might be better to deal with them by pre-
                     # compiling them away?
                     raise RuntimeError('Weird precondition atom "X=Y". Might be worth inspecting it.')
+            eq_constraints += [lor(neg(l), neg(r))]
+    return eq_constraints
 
 
 def create_grounding_constraints_with_statics(count, selects, statics, init, precs, constraints, state_atoms):
@@ -113,30 +114,23 @@ def create_grounding_constraints_with_statics(count, selects, statics, init, pre
             head = p.expr.predicate(*values)
             body = [neg(h) for h in body]
             arity = len(body)
-            if len(body) == 1:
-                body = body[0]
+            # if len(body) == 1:
+            #     body = body[0]
 
-            if is_static(p.expr, statics):
+            if not is_static(p.expr, statics):
                 if symref(head) not in state_atoms:
                     state_atoms.add(symref(head))
-                if arity == 0:  # 0-arity predicate
-                    constraints += [head]
-                    continue
-                try:
-                    body = lor(*body)
-                except TypeError:
-                    pass
-                constraints += [lor(body, head)]
+                constraints.append(lor(*body, head) if arity > 0 else head)
                 continue
 
             if init[head]:  # If the atom is static and holds in the initial state, no grounding constraint is necessary
                 continue
 
-            # only selects
-            try:
-                constraints += [lor(*body)]
-            except TypeError:
+            assert body
+            if len(body) == 1:
                 constraints += [body]
+            else:
+                constraints += [lor(*body)]
 
 
 def create_select_atoms(action):
@@ -205,139 +199,123 @@ def _extract_equalities_rec(phi, table):
             raise RuntimeError('Unsupported formula: {}'.format(str(phi)))
 
 
-def process_problem(the_task):
-    # setup model evaluator
-    the_task.init.evaluator = evaluate
+def process_schema(problem, statics, action, data):
+    print(f"Processing action schema:\n{action}")
+    data['instance'] += [problem.name]
+    nvars = 0
+    selects, reified, n = create_select_atoms(action)
+    nvars += n
+    data['selects'] += [nvars]
+    dom_constraints = []
+    create_domain_constraints(action, selects, dom_constraints)
+    equalities = extract_equalities(action.precondition)
+    eq_constraints = create_equality_constraints(equalities, selects)
+    state_atoms = set()
+    grounding_constraints = []
+    prec_atoms = set()
+    extract_regular_atoms(action.precondition, prec_atoms)
+    # Create the constraints of the form
+    #     select(X1, x1) and ... and select (Xn, xn) --> p(x1, ..., xn)
+    # for each atom p in the precondition conjunction.
+    count = OrderedDict({x: 0 for x in selects})
+    # create_grounding_constraints(count, S_a, prec_atoms, grounding_constraints, state_atoms)
+    create_grounding_constraints_with_statics(count, selects, statics, problem.init, prec_atoms,
+                                              grounding_constraints, state_atoms)
+    nvars += len(state_atoms)
 
-    data = defaultdict(list)
-    actions = scout_actions(the_task, data)
-    statics = compute_statics(the_task)
+    data['atoms'] += [len(state_atoms)]
+    data['DOM'] += [len(dom_constraints)]
+    data['EQ'] += [len(eq_constraints)]
+    data['GROUND'] += [len(grounding_constraints)]
+    data['nclauses'] += [len(dom_constraints) + len(eq_constraints) + len(grounding_constraints)]
+    data['nvars'] += [nvars]
 
-    for actions in actions:
-        data['instance'] += [the_task.name]
-        nvars = 0
-        selects, reified, n = create_select_atoms(actions)
-        nvars += n
-        data['selects'] += [nvars]
-        dom_constraints = []
-        create_domain_constraints(actions, selects, dom_constraints)
-        equalities = extract_equalities(actions.precondition)
-        eq_constraints = []
-        create_equality_constraints(equalities, selects, eq_constraints)
-        state_atoms = set()
-        grounding_constraints = []
-        prec_atoms = set()
-        extract_regular_atoms(actions.precondition, prec_atoms)
-        # Create the constraints of the form
-        #     select(X1, x1) and ... and select (Xn, xn) --> p(x1, ..., xn)
-        # for each atom p in the precondition conjunction.
-        count = OrderedDict({x: 0 for x in selects})
-        # create_grounding_constraints(count, S_a, prec_atoms, grounding_constraints, state_atoms)
-        create_grounding_constraints_with_statics(count, selects, statics, the_task.init, prec_atoms,
-                                                  grounding_constraints, state_atoms)
-        nvars += len(state_atoms)
+    symbols = {}
+    sdd_var_id = 1
+    inv_symbols = {}
 
-        data['atoms'] += [len(state_atoms)]
-        data['DOM'] += [len(dom_constraints)]
-        data['EQ'] += [len(eq_constraints)]
-        data['GROUND'] += [len(grounding_constraints)]
-        data['nclauses'] += [len(dom_constraints) + len(eq_constraints) + len(grounding_constraints)]
-        data['nvars'] += [nvars]
-
-        symbols = {}
-        sdd_var_id = 1
-        inv_symbols = {}
-
-        scored_x = [(v, x) for x, v in count.items()]
-        scored_x.sort(key=lambda x: x[0], reverse=True)
-        # print(scored_x)
-        for _, x in scored_x:
-            # print(S_a[x])
-            for s in selects[x]:
-                symbols[symref(s)] = sdd_var_id
-                inv_symbols[sdd_var_id] = s
-                sdd_var_id += 1
-        for p in state_atoms:
-            symbols[p] = sdd_var_id
-            inv_symbols[sdd_var_id] = p
+    scored_x = [(v, x) for x, v in count.items()]
+    scored_x.sort(key=lambda x: x[0], reverse=True)
+    # print(scored_x)
+    for _, x in scored_x:
+        # print(S_a[x])
+        for s in selects[x]:
+            symbols[symref(s)] = sdd_var_id
+            inv_symbols[sdd_var_id] = s
             sdd_var_id += 1
+    for p in state_atoms:
+        symbols[p] = sdd_var_id
+        inv_symbols[sdd_var_id] = p
+        sdd_var_id += 1
 
-        # set up vtree and manager
-        var_order = list(range(1, nvars + 1))  # lexicographic
-        # var_order = list(range(nvars,0,-1)) # state atoms first
-        vtree_type = "right"  # OBDD-style
+    manager = setup_sdd_manager(nvars)
 
-        vtree = Vtree(nvars, var_order, vtree_type)
-        manager = SddManager(var_count=nvars, auto_gc_and_minimize=1, vtree=vtree)
+    sdd_ground = [translate_to_pysdd(c, symbols, manager) for c in grounding_constraints]
+    sdd_eq = [translate_to_pysdd(c, symbols, manager) for c in eq_constraints]
+    sdd_dom = [translate_to_pysdd(c, symbols, manager) for c in dom_constraints]
+    sdd_pre = sdd_dom[0]
+    sdd_sizes = []
 
-        sdd_ground = [translate(c, symbols, manager) for c in grounding_constraints]
-        sdd_eq = [translate(c, symbols, manager) for c in eq_constraints]
-        sdd_dom = [translate(c, symbols, manager) for c in dom_constraints]
-        sdd_pre = sdd_dom[0]
-        sdd_sizes = []
+    max_size = 20000000
+    failed = False
+    # t0 = time.time()
+    try:
 
-        max_size = 20000000
-        failed = False
-        # t0 = time.time()
-        try:
+        for i in range(1, len(sdd_dom)):
+            sdd_pre = sdd_pre & sdd_dom[i]
+            sdd_sizes += [sdd_pre.size()]
+            if sdd_sizes[-1] > max_size:
+                raise MaxSizeError()
 
-            for i in range(1, len(sdd_dom)):
-                sdd_pre = sdd_pre & sdd_dom[i]
-                sdd_sizes += [sdd_pre.size()]
-                if sdd_sizes[-1] > max_size:
-                    raise MaxSizeError()
+        for i in range(len(sdd_eq)):
+            sdd_pre = sdd_pre & sdd_eq[i]
+            sdd_sizes += [sdd_pre.size()]
+            if sdd_sizes[-1] > max_size:
+                raise MaxSizeError()
 
-            for i in range(len(sdd_eq)):
-                sdd_pre = sdd_pre & sdd_eq[i]
-                sdd_sizes += [sdd_pre.size()]
-                if sdd_sizes[-1] > max_size:
-                    raise MaxSizeError()
+        for i in range(len(sdd_ground)):
+            sdd_pre = sdd_pre & sdd_ground[i]
+            sdd_sizes += [sdd_pre.size()]
+            if sdd_sizes[-1] > max_size:
+                raise MaxSizeError()
 
-            for i in range(len(sdd_ground)):
-                sdd_pre = sdd_pre & sdd_ground[i]
-                sdd_sizes += [sdd_pre.size()]
-                if sdd_sizes[-1] > max_size:
-                    raise MaxSizeError()
+    except MaxSizeError:
+        failed = True
+    # tf = time.time()
+    # data_df['runtime'] += [tf - t0]
 
-        except MaxSizeError:
-            failed = True
-            pass
-        # tf = time.time()
-        # data_df['runtime'] += [tf - t0]
+    # sdd_clauses = sdd_ground + sdd_eq + sdd_dom
+    # plot_sdd_size(the_task.domain_name, the_task.name, act, sdd_sizes, sdd_clauses, sdd_eq, sdd_ground, sdd_dom)
 
-        # sdd_clauses = sdd_ground + sdd_eq + sdd_dom
-        # plot_sdd_size(the_task.domain_name, the_task.name, act, sdd_sizes, sdd_clauses, sdd_eq, sdd_ground, sdd_dom)
+    data['sdd_sizes'] += [sdd_sizes]
+    data['sdd_size'] += [sdd_sizes[-1]]
 
-        data['sdd_sizes'] += [sdd_sizes]
-        data['sdd_size'] += [sdd_sizes[-1]]
-
-        if not failed:
-            s = []
-            for p in state_atoms:
-                if the_task.init[p.expr]:
-                    s += [p.expr]
-                else:
-                    s += [neg(p.expr)]
-            # MRJ: code below is good but if state has more than 255 literals we
-            # exceed the maximal recursion depth allowed by the interpreter
-            # s = land(*s)
-            # sdd_s = translate(s, symbols, manager)
-            s = [translate(l, symbols, manager) for l in s]
-            sdd_s = s[0]
-            for l in s[1:]:
-                sdd_s = sdd_s & l
-            app = sdd_pre & sdd_s
-            wmc = app.wmc(log_mode=False)
-            wmc.propagate()
-            data['A(s0)'] += [app.model_count()]
-        else:
-            data['A(s0)'] += [None]
-    return data
+    if not failed:
+        s = []
+        for p in state_atoms:
+            if problem.init[p.expr]:
+                s += [p.expr]
+            else:
+                s += [neg(p.expr)]
+        # MRJ: code below is good but if state has more than 255 literals we
+        # exceed the maximal recursion depth allowed by the interpreter
+        # s = land(*s)
+        # sdd_s = translate(s, symbols, manager)
+        s = [translate_to_pysdd(l, symbols, manager) for l in s]
+        sdd_s = s[0]
+        for l in s[1:]:
+            sdd_s = sdd_s & l
+        app = sdd_pre & sdd_s
+        wmc = app.wmc(log_mode=False)
+        wmc.propagate()
+        data['A(s0)'] += [app.model_count()]
+    else:
+        data['A(s0)'] += [None]
 
 
-def translate(phi, syms, manager):
+def translate_to_pysdd(phi, syms, manager):
     if isinstance(phi, CompoundFormula):
-        sub = [translate(psi, syms, manager) for psi in phi.subformulas]
+        sub = [translate_to_pysdd(psi, syms, manager) for psi in phi.subformulas]
         if phi.connective == Connective.Not:
             return -sub[0]
         elif phi.connective == Connective.And:
@@ -348,3 +326,27 @@ def translate(phi, syms, manager):
         sdd_lit = manager.literal(syms[symref(phi)])
         return sdd_lit
     raise RuntimeError(f"Could not translate {phi}")
+
+
+def setup_sdd_manager(nvars):
+    """ Set up the SDD manager with some sensible defaults"""
+    # set up vtree and manager
+    var_order = list(range(1, nvars + 1))  # lexicographic
+    # var_order = list(range(nvars,0,-1)) # state atoms first
+    vtree_type = "right"  # OBDD-style
+
+    vtree = Vtree(nvars, var_order, vtree_type)
+    return SddManager(var_count=nvars, auto_gc_and_minimize=1, vtree=vtree)
+
+
+def process_problem(problem):
+    # Make sure the initial state has some associated evaluator:
+    problem.init.evaluator = problem.init.evaluator or evaluate
+
+    data = defaultdict(list)
+    actions = scout_actions(problem, data)
+    statics = compute_statics(problem)
+
+    for action in actions:
+        process_schema(problem, statics, action, data)
+    return data
