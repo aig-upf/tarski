a13caa62902d3099f7b776d778769c0c90a7ee47
Guillem Frances <guillem.frances@gmail.com>
2019-12-04 14:33:05 +0100

    Fix style checks that were erroring on travis

--- src/tarski/syntax/algebra/matrix.py
+++ src/tarski/syntax/algebra/matrix.py
@@ -10,6 +10,7 @@ class Matrix(Term):
         self.matrix = np.array(arraylike, dtype=np.dtype(object))
         self._sort = sort
         # verify and cast
+        # pylint: disable=unpacking-non-sequence
         N, M = self.matrix.shape
         for i in range(N):
             for j in range(M):
@@ -47,6 +48,7 @@ class Matrix(Term):
     __repr__ = __str__
 
     def hash(self):
+        # pylint: disable=unpacking-non-sequence
         N, M = self.matrix.shape
         syms = []
         for i in range(N):
@@ -57,6 +59,7 @@ class Matrix(Term):
     def is_syntactically_equal(self, other):
         if self.__class__ is not other.__class__ or self.matrix.shape != other.matrix.shape:
             return False
+        # pylint: disable=unpacking-non-sequence
         N, M = self.matrix.shape
         for i in range(N):
             for j in range(M):
