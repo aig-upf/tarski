ea8a437ba66866d5538cfe685f793e27a6988cd9
Guillem Frances <guillem.frances@gmail.com>
2020-03-11 16:37:27 +0100

    Update supported Python versions

--- setup.py
+++ setup.py
@@ -41,8 +41,10 @@ def main():
             'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
             'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
+            'Programming Language :: Python :: 3.8',
         ],
 
         packages=find_packages('src'),  # include all packages under src
