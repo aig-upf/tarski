721d8e342146987c250f4848ffc9c1e07b3df5ec
Guillem Frances <guillem.frances@gmail.com>
2020-03-11 17:14:00 +0100

    Update supported Python versions

--- setup.py
+++ setup.py
@@ -41,7 +41,6 @@ def main():
             'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.5',
             'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
