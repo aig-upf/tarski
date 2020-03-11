b80a9fbbc97825d37aabbe428888a9bd357948e3
Guillem Frances <guillem.frances@gmail.com>
2020-03-11 12:31:30 +0100

    Remove buggy method that couldn't possibly be being used

--- src/tarski/fstrips/action.py
+++ src/tarski/fstrips/action.py
@@ -16,12 +16,6 @@ class Action:
     def __lt__(self, other):
         return self.name < other.name
 
-    def dump(self):
-        return dict(name=self.name,
-                    params=[par.dump() for par in self.parameters],
-                    precondition=self.precondition.dump(),
-                    effects=[eff.dump() for eff in self.effects.dump()])
-
     def ident(self):
         paramlist = "{}".format(','.join("{}: {}".format(p.symbol, p.sort.name) for p in self.parameters))
         return f'{self.name}({paramlist})'
