85697772d7f41c6647cd7f8cc412a4159ae36dbf
Guillem Frances <guillem.frances@gmail.com>
2020-04-17 11:11:03 +0200

    Fix tests

--- tests/analysis/test_csp_schema.py
+++ tests/analysis/test_csp_schema.py
@@ -3,7 +3,7 @@ import tempfile
 from tarski.analysis.csp_schema import CSPCompiler, CSPInformation
 from tarski.benchmarks.blocksworld import generate_strips_blocksworld_problem
 from tarski.grounding import NaiveGroundingStrategy
-
+from tarski.syntax.ops import compute_sort_id_assignment
 
 
 def test_bw_csp():
@@ -15,13 +15,14 @@ def test_bw_csp():
 
     grounding = NaiveGroundingStrategy(problem, ignore_symbols={'total-cost'})
     state_variables = grounding.ground_state_variables()
+    sort_bounds, object_ids = compute_sort_id_assignment(problem.language)
 
-    comp = CSPCompiler(problem, state_variables)
+    comp = CSPCompiler(problem, state_variables, sort_bounds, object_ids)
     csp = CSPInformation()
     comp.compile_expression(on(b1, b2) & clear(x), csp)
     assert len(csp.constraints) == 2 and \
         str(csp.constraints[0]) == 'StateVar(on(b1,b2)=True, reif=None)' and \
-        str(csp.constraints[1]) == 'Table(<x>, ext(clear), reif=None)'
+        str(csp.constraints[1]) == 'Table(<x> in ext(clear), negative=False, reif=None)'
 
     csp = CSPInformation()
     comp.compile_expression(handempty() & ~handempty(), csp)
@@ -36,6 +37,8 @@ def test_bw_csp_full():
     grounding = NaiveGroundingStrategy(problem, ignore_symbols={'total-cost'})
     state_variables = grounding.ground_state_variables()
 
-    comp = CSPCompiler(problem, state_variables)
+    sort_bounds, object_ids = compute_sort_id_assignment(problem.language)
+
+    comp = CSPCompiler(problem, state_variables, sort_bounds, object_ids)
     with tempfile.TemporaryDirectory() as dirpath:
         _ = comp.process_problem(serialization_directory=dirpath)
