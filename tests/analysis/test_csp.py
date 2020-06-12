41fce7071f92c3ecb54979817c0054b11f148ed2
Guillem Frances <guillem.frances@gmail.com>
2020-06-12 15:13:23 +0200

    Add test for schema hypergraph computation

--- tests/analysis/test_csp.py
+++ tests/analysis/test_csp.py
@@ -6,11 +6,15 @@ from tests.io.common import collect_strips_benchmarks, reader
 
 
 def test_hypergraph_generation():
-    instance_file, domain_file = collect_strips_benchmarks(["pipesworld-notankage:p01-net1-b6-g2.pddl"])[0]
-    problem = reader().read_problem(domain_file, instance_file)
-    hgraphs = {a.name: compute_schema_constraint_hypergraph(a) for a in problem.actions.values()}
-    acyclicity = {name: check_hypergraph_acyclicity(h) for name, h in hgraphs.items()}
+    def compute_acyclicity(instance):
+        instance_file, domain_file = collect_strips_benchmarks([instance])[0]
+        problem = reader(case_insensitive=True).read_problem(domain_file, instance_file)
+        hgraphs = {a.name: compute_schema_constraint_hypergraph(a) for a in problem.actions.values()}
+        return {name: check_hypergraph_acyclicity(h) for name, h in hgraphs.items()}
 
+    acyclicity = compute_acyclicity("pipesworld-notankage:p01-net1-b6-g2.pddl")
     assert acyclicity == {'PUSH-START': False, 'PUSH-END': True, 'POP-START': False,
                           'POP-END': True, 'PUSH-UNITARYPIPE': False, 'POP-UNITARYPIPE': False}
 
+    acyclicity = compute_acyclicity("ged-opt14-strips:d-1-2.pddl")
+    assert all(x is True for x in acyclicity.values())
