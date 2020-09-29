c16ef4a20aad7d6dc6f09d8941b0d61807d94a47
Guillem Frances <guillem.frances@gmail.com>
2020-09-29 13:22:14 +0200

    Fix bug in check_hypergraph_acyclicity

--- src/tarski/analysis/csp.py
+++ src/tarski/analysis/csp.py
@@ -37,21 +37,44 @@ def compute_primal_graph_from_hypergraph(hypergraph):
 
 
 def _remove_ear_if_exists(edges, node_counts):
-    """ A helper to find a so-called ear of the hypergraph """
-    for e1, e2 in itertools.product(edges, edges):
-        diff = e1-e2
-        if diff and all(node_counts[node] == 1 for node in diff):
-            # e1 is an ear and can be removed. We update node count as well
-            for node in e1:
-                node_counts[node] -= 1
-            edges.remove(e1)
+    """ Find and remove from the given set of edges a so-called "ear" and return True.
+    If no such ear exists, return False.
+
+    From Abiteboul, S., Hull, R. and Vianu, V (1995). Foundations of Databases:
+        An ear of hypergraph F = (V, F) is an edge f ∈ F such that for some distinct f' ∈ F,
+        no vertex of f − f' is in any other edge [...]. In this case, f' is called a witness that f is an ear.
+        As a special case, if there is an edge f of F that intersects no other edge, then f is also considered an ear.
+
+    Note that this implementation is possibly not the most efficient, but it should work fine for the kind of small
+    hypergraphs we're interested in.
+    """
+    def remove_edge(edge):
+        for node in edge:
+            node_counts[node] -= 1
+        edges.remove(edge)
+
+    for f in edges:
+        # Check first the "special case"
+        if all(node_counts[node] == 1 for node in f):
+            remove_edge(f)
+            # print(f'Edge {f} removed because it intersects no other edge')
             return True
+
+    for f, fprime in itertools.permutations(edges, 2):
+        # Note that this includes the case where f \subseteq f'.
+        # Abiteboul et al. assume that hypergraph is "reduced" and f has thus been compiled into f' at preprocessing;
+        # we simply compile it away here on the fly by assuming it is indeed an ear and can be removed.
+        if all(node_counts[node] == 1 for node in f-fprime):
+            remove_edge(f)
+            # print(f'Edge {f} removed in favor of {fprime}')
+            return True
+    # print(f'No more ears found in {edges}')
     return False
 
 
 def check_hypergraph_acyclicity(hypergraph):
     """ Check whether the given hypergraph is acyclic by applying the GYO reduction as described in
-        Jeffrey D. Ullman, Principles of Database and Knowledge-Base Systems, Vol. II
+        Abiteboul, S., Hull, R. and Vianu, V (1995). Foundations of Databases, pp.131-132.
     """
     nodes = set(itertools.chain.from_iterable(hypergraph))
     edges = set(frozenset(x) for x in hypergraph)  # simply convert the tuple into frozensets
@@ -64,10 +87,11 @@ def check_hypergraph_acyclicity(hypergraph):
         for n in edge:
             node_counts[n] += 1
 
+    # This is essentially the GYO algorithm:
     while _remove_ear_if_exists(edges, node_counts):
         pass
 
-    return len(edges) <= 1
+    return len(edges) == 0
 
 
 def _collect_hyperedges(phi, edges):
