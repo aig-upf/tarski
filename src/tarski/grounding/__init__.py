0b01249f0a00d55f02d405cc3c29d9bf9e82da55
Miquel Ramírez <miquel.ramirez@gmail.com>
2021-04-01 12:55:19 +1100

    Minor

--- src/tarski/grounding/__init__.py
+++ src/tarski/grounding/__init__.py
@@ -1,3 +1,4 @@
 
-from .naive_grounding import ProblemGrounding, create_all_possible_state_variables, NaiveGroundingStrategy
+from .naive_grounding import ProblemGrounding, create_all_possible_state_variables, \
+    NaiveGroundingStrategy
 from .lp_grounding import LPGroundingStrategy
