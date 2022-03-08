c01b54e331241ac4f479098cc511d20c7b211558
Guillem Frances <guillem.frances@gmail.com>
2022-03-08 16:02:24 +0100

    Update metadata on supported Python versions

--- setup.py
+++ setup.py
@@ -41,10 +41,10 @@ def main():
             "License :: OSI Approved :: Apache Software License",
 
             'Programming Language :: Python :: 3',
-            'Programming Language :: Python :: 3.6',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
             'Programming Language :: Python :: 3.9',
+            'Programming Language :: Python :: 3.10',
         ],
 
         packages=find_packages('src'),  # include all packages under src
